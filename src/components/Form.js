8f06f7de8a868abe992e72b02e59f2a1f3f9675f
Justin Hurst <hurstjustin502@gmail.com>
2021-06-03 14:57:02 -0400

    stateless form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,44 @@
 import React from 'react'
 import { navigate } from 'gatsby-link'
+import ContactForm from './ContactForm';
 
-function encode(data) {
-    return Object.keys(data)
-        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-        .join('&')
-}
+// function encode(data) {
+//     return Object.keys(data)
+//         .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+//         .join('&')
+// }
 
-export default function ContactFormActual() {
-    const [state, setState] = React.useState({})
+// export default function ContactFormActual() {
+//     const [state, setState] = React.useState({})
 
-    const handleChange = (e) => {
-        setState({ ...state, [e.target.name]: e.target.value })
-    }
+//     const handleChange = (e) => {
+//         setState({ ...state, [e.target.name]: e.target.value })
+//     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const form = e.target
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({
-                'form-name': form.getAttribute('name'),
-                ...state,
-            }),
-        })
-            .then(() => navigate(form.getAttribute('action')))
-            .catch((error) => alert(error))
-    }
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//         const form = e.target
+//         fetch('/', {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//             body: encode({
+//                 'form-name': form.getAttribute('name'),
+//                 ...state,
+//             }),
+//         })
+//             .then(() => navigate(form.getAttribute('action')))
+//             .catch((error) => alert(error))
+//     }
+const ContactFormActual = () => {
 
 
     return (
             <form
                 name="contact"
                 method="post"
+                action="/thanks/"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
-                onSubmit={handleSubmit}
             >
 
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
@@ -45,19 +47,19 @@ export default function ContactFormActual() {
                 <div style={{ paddingTop: 10, paddingBottom: 10 }}>
                     <label >
                         Name
-                            <input type="text" name="name" className="name-input" onChange={handleChange} />
+                            <input type="text" name="name" className="name-input" />
                     </label>
                 </div>
                 <div style={{ paddingTop: 10, paddingBottom: 10 }}>
                     <label >
                         Email
-                            <input type="email" name="email" className="email-input" onChange={handleChange} />
+                            <input type="email" name="email" className="email-input"/>
                     </label>
                 </div>
                 <div style={{ paddingTop: 10, paddingBottom: 10, display: 'flex', flexDirection: 'column' }}>
                     <label style={{ justifySelf: 'flex-start' }}>
                         Message
-                            <textarea type="textarea" name="message" className="message-input" onChange={handleChange} />
+                            <textarea type="textarea" name="message" className="message-input" />
                     </label>
                 </div>
                 <div style={{ paddingTop: 45, paddingBottom: 20, paddingLeft: 35, paddingRight: 20 }}>
@@ -66,3 +68,5 @@ export default function ContactFormActual() {
             </form>
     );
 }
+
+export default ContactFormActual;
