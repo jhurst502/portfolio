1f4599de1a746fb7f75da5b1123a80bcd71ec4b3
Justin Hurst <hurstjustin502@gmail.com>
2021-06-04 15:14:51 -0400

    close button on contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,9 +11,12 @@ const ContactForm = ({ open, onClose }) => {
         <>
             <div className="overlay">
                 <div className="contact-form">
+                    <div style={{paddingLeft:'100%'}}>
                     <button onClick={onClose} className="close-button">
-                        <img src={close} alt="close window"/>
+                        <img src={close} className="close-button" alt="close window"/>
                     </button>
+                    </div>
+                    
                     <h3 style={{ paddingTop: 20 }}>Have questions? Want a quote? Send me a message! </h3>
                     <ContactFormActual></ContactFormActual>
                 </div>
